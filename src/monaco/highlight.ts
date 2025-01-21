859f69667ddd02a6b2e3a130353eb57c40ffceaf
_lmmmmmm <lmmmmmm12138@gmail.com>
2025-01-21 11:39:47 +0800

    chore(shiki): bump to v2

--- src/monaco/highlight.ts
+++ src/monaco/highlight.ts
@@ -2,16 +2,16 @@ import { shikiToMonaco } from '@shikijs/monaco';
 import { emmetCSS, emmetJSX } from 'emmet-monaco-es';
 import * as monaco from 'monaco-editor';
 import { createHighlighterCore } from 'shiki/core';
+import { createOnigurumaEngine } from 'shiki/engine/oniguruma';
 import LangCss from 'shiki/langs/css.mjs';
 import LangJson from 'shiki/langs/json.mjs';
 import LangJsx from 'shiki/langs/jsx.mjs';
 import LangTsx from 'shiki/langs/tsx.mjs';
 import VitesseDark from 'shiki/themes/vitesse-dark.mjs';
 import VitesseLight from 'shiki/themes/vitesse-light.mjs';
-import wasmInlined from 'shiki/wasm';
 
 const highlighter = await createHighlighterCore({
-  loadWasm: wasmInlined,
+  engine: createOnigurumaEngine(() => import('shiki/wasm')),
   themes: [VitesseLight, VitesseDark],
   langs: [LangTsx, LangJsx, LangJson, LangCss],
 });
